1b6f4f511e037ec5badc40db29af24b2c0bc08db
a-nacef <aziznacef2@gmail.com>
2022-03-25 11:10:55 +0100

    Debugging producer in compose

--- Producer/Producer.py
+++ Producer/Producer.py
@@ -1,8 +1,7 @@
-from distutils.log import error
 from encodings import utf_8
-from gc import callbacks
 from time import sleep
 from confluent_kafka import Producer
+import confluent_kafka
 from produce_record import produce
 import logging
 import os
@@ -22,29 +21,29 @@ def on_delivery(err, msg):
     if err:
         logging.exception(f"Message delivery failed with err: {err}")
     else:
-        print(f"Message succesfully delivered, topic = {msg.topic}, partition = {msg.partition}")
+        logging.info(f"Message succesfully delivered, topic = {msg.topic}, partition = {msg.partition}")
+
+try:
+    Prod = Producer({
+        "bootstrap.servers":os.environ["KAFKA_STRING"]
+    })
+except Exception:
+    logging.critical("Couldn't connect to the broker")
+    exit(1)
 
 
-Prod = Producer({
-    "bootstrap.servers":"kafka:9092"
-})
+try:
+  while True:
+      data,key= produce()
+      sleep(config["Interval"])
+      Prod.produce('Transactions', data.encode("utf-8"), key=key)
 
-
-
-if __name__== "__main__":
-    try:
-        while True:
-            data,key= produce()
-            sleep(config["Interval"])
-            Prod.produce('Transactions', data.encode("utf-8"), key=key)
-
-
-    except error:
-        logging.critical(f"Producer failed with err = {error}")
-        Prod.flush()
-    except KeyboardInterrupt:
-        Prod.flush()
-        logging.info("Producer interrupted, exiting.")
+except Exception:
+  logging.critical(f"Producer failed.")
+  Prod.flush()
+except KeyboardInterrupt:
+  Prod.flush()
+  logging.info("Producer interrupted, exiting.")
 
 
 
